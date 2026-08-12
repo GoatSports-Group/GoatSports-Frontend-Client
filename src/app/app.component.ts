8e33015d6df95dfba8b7af6cce80f1698312e923
MinhDat1312 <nguyenthangdat84@gmail.com>
2026-08-12 18:30:38 +0700

    style(client): remove unused app.component.scss stylesheet reference

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,9 @@ import { AuthService } from './presentation/services/auth.service';
 import { Observable } from 'rxjs';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: false
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false
 })
 export class AppComponent implements OnInit {
   title = 'Goat Sports';
